ae8fa0e09c9c48f80fc673c7cf005852ba29ebd9
kelvin <kelvin.musyoka@student.moringaschool.com>
2023-12-16 14:29:01 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+  return `This Saturday, I want to ${activity}!`
+}
+
+
+function mondayWork(schedule = "go to the office"){
+  return `This Monday, I will ${schedule}.`
+}
+function wrapAdjective(flair ="*"){
+  return function (pronoun="special"){
+    return (`You are ${flair+pronoun+flair}!`)
+  }
+  
+}
